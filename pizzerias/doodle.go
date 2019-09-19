50b40d52e856048897c7cf526aa9d5b1e88ab717
Roberto Santalla <roobre@roobre.es>
2019-09-19 14:12:00 +0200

    Better implementation for doodle-based menu

--- pizzerias/doodle.go
+++ pizzerias/doodle.go
@@ -5,13 +5,28 @@ import (
 	"roob.re/pizzameeting/doodle"
 )
 
-func FromDoodle(d *doodle.Doodle) pizzameeting.Pizzeria {
-	return pizzameeting.PizzeriaFunc(func() []pizzameeting.Pizza {
-		var menu []pizzameeting.Pizza
-		for _, opt := range d.Options() {
-			menu = append(menu, pizzameeting.Pizza(opt))
-		}
+type DoodlePizzeria struct {
+	doodle *doodle.Doodle
+}
 
-		return menu
-	})
+func (dp *DoodlePizzeria) Menu() []pizzameeting.Pizza {
+	var menu []pizzameeting.Pizza
+	for _, opt := range dp.doodle.Options() {
+		menu = append(menu, pizzameeting.Pizza(opt))
+	}
+
+	return menu
+}
+
+func (dp *DoodlePizzeria) StringMenu() []string {
+	var menu []string
+	for _, opt := range dp.doodle.Options() {
+		menu = append(menu, string(opt))
+	}
+
+	return menu
+}
+
+func FromDoodle(d *doodle.Doodle) pizzameeting.Pizzeria {
+	return &DoodlePizzeria{doodle: d}
 }
