cc24f93e1d9a091a3363193551e227925d8c02b1
Roberto Santalla <roobre@roobre.es>
2019-09-19 14:13:09 +0200

    Use an slice instead of a map for faster penalties storage

--- person.go
+++ person.go
@@ -7,7 +7,7 @@ import (
 const minScore = 10
 const maxScore = 2*minScore - 1
 
-const repeatPenaltyFactor = 0.66 // Score will be multiplied by 1-repeatPenaltyFactor. The higher the factor, the more the penalty
+const repeatPenaltyFactor = 0.60 // Score will be multiplied by 1-repeatPenaltyFactor. The higher the factor, the more the penalty
 
 type Attendee interface {
 	Evaluate([]Pizza) int
@@ -15,26 +15,52 @@ type Attendee interface {
 
 type Pizza string
 
-type Person struct {
-	Id     Identifier `json:"-"`
-	Name   string
-	scores map[Pizza]int
+type Score struct {
+	pizza Pizza
+	score int
 }
 
-func (p *Person) Score(pi Pizza, score int) {
+type Person struct {
+	Id          Identifier `json:"-"`
+	Name        string
+	scores      map[Pizza]int
+	scoresSlice []Score
+}
+
+func (p *Person) Score(pizza Pizza, score int) {
 	if p.scores == nil {
 		p.scores = make(map[Pizza]int)
 	}
+	p.scores[pizza] = score
 
-	p.scores[pi] = score
+	p.scoresSlice = append(p.scoresSlice, Score{pizza, score})
+}
+
+func (p *Person) pizzaScore(pizza Pizza) int {
+	if len(p.scoresSlice) < 16 {
+		for _, score := range p.scoresSlice {
+			if score.pizza == pizza {
+				return score.score
+			}
+		}
+		return 0
+	} else {
+		return p.scores[pizza]
+	}
 }
 
 func (p *Person) Evaluate(menu []Pizza) int {
 	var score float32 = 0
-	repeatPenalties := make(map[Pizza]float32, 4)
+	//repeatPenalties := make(map[Pizza]float32, len(menu))
+	penalized := make([]Pizza, 0, len(menu))
+
 	for _, pizza := range menu {
-		score += float32(p.scores[pizza]) * (repeatPenalties[pizza] + 1)
-		repeatPenalties[pizza] = -repeatPenaltyFactor
+		// Accessing maps is cheap, appending is not
+		score += float32(p.scores[pizza]) * penalty(pizza, penalized)
+		//score += float32(p.scores[pizza]) * (repeatPenalties[pizza] + 1)
+		//score += float32(p.pizzaScore(pizza)) * (repeatPenalties[pizza] + 1)
+		penalized = append(penalized, pizza)
+		//repeatPenalties[pizza] = -repeatPenaltyFactor
 	}
 
 	if len(p.scores) >= 2 {
@@ -42,4 +68,14 @@ func (p *Person) Evaluate(menu []Pizza) int {
 	} else {
 		return int(math.Round(float64(score))) - (minScore - 1)
 	}
+}
+
+func penalty(pizza Pizza, penalties []Pizza) float32 {
+	for _, penalizedPizza := range penalties {
+		if penalizedPizza == pizza {
+			return 1 - repeatPenaltyFactor
+		}
+	}
+
+	return 1
 }
